27b52945fda95f6226ca84024e75d05e5e313ed0
stratzoomer <stratzoomer@users.noreply.github.com>
2014-03-31 20:12:26 -0400

    Working freemarker

--- src/main/java/com/brahma/loganalyzer/LogAnalyzerApplication.java
+++ src/main/java/com/brahma/loganalyzer/LogAnalyzerApplication.java
@@ -2,35 +2,49 @@ package com.brahma.loganalyzer;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.views.ViewBundle;
 import io.dropwizard.setup.Environment;
+
+import org.slf4j.Logger; 
+import org.slf4j.LoggerFactory;
+
+import com.brahma.loganalyzer.resources.DropWizardExampleResource;
 import com.brahma.loganalyzer.resources.LogAnalyzerMainResource;
 import com.brahma.loganalyzer.health.TemplateHealthCheck;
+import com.brahma.loganalyzer.core.Contact;
 
 public class LogAnalyzerApplication extends Application<LogAnalyzerConfiguration> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogAnalyzerApplication.class);
+	private Contact contact;
+
     public static void main(String[] args) throws Exception {
         new LogAnalyzerApplication().run(args);
     }
 
     @Override
     public String getName() {
-        return "hello-world";
+        return "Log Analyzer";
     }
 
     @Override
     public void initialize(Bootstrap<LogAnalyzerConfiguration> bootstrap) {
-        // nothing to do yet
+        bootstrap.addBundle(new ViewBundle());
     }
 
     @Override
     public void run(LogAnalyzerConfiguration configuration,
                     Environment environment) {
-        final LogAnalyzerMainResource resource = new LogAnalyzerMainResource(
+    	LOGGER.info("Inside run of LogAnalyzer");
+        final DropWizardExampleResource exampleResource = new DropWizardExampleResource(
                 configuration.getTemplate(),
                 configuration.getDefaultName()
             );
+        contact = new Contact(100, "Brahma", "Koodallur", "555-555-5555");
+        final LogAnalyzerMainResource mainResource = new LogAnalyzerMainResource(contact);
             final TemplateHealthCheck healthCheck =
                     new TemplateHealthCheck(configuration.getTemplate());
             environment.healthChecks().register("template", healthCheck);            
-            environment.jersey().register(resource);
+            environment.jersey().register(exampleResource);
+            environment.jersey().register(mainResource);
     }
 }
--- src/main/java/com/brahma/loganalyzer/resources/LogAnalyzerMainResource.java
+++ src/main/java/com/brahma/loganalyzer/resources/LogAnalyzerMainResource.java
@@ -1,33 +1,25 @@
 package com.brahma.loganalyzer.resources;
 
-import com.brahma.loganalyzer.core.Saying;
-import com.google.common.base.Optional;
-import com.codahale.metrics.annotation.Timed;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.concurrent.atomic.AtomicLong;
+
+import com.brahma.loganalyzer.views.LogAnalyzerMainView;
+import com.brahma.loganalyzer.core.Contact;
 
 @Path("/loganalyzer")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_HTML)
 public class LogAnalyzerMainResource {
-    private final String template;
-    private final String defaultName;
-    private final AtomicLong counter;
+	private final Contact contact; 
+	
+    public LogAnalyzerMainResource(Contact contact) {
+		this.contact = contact;
+	}
 
-    public LogAnalyzerMainResource(String template, String defaultName) {
-        this.template = template;
-        this.defaultName = defaultName;
-        this.counter = new AtomicLong();
+	@GET
+    public LogAnalyzerMainView showResponse() {
+        return new LogAnalyzerMainView(contact);
     }
 
-    @GET
-    @Timed
-    public Saying sayHello(@QueryParam("name") Optional<String> name) {
-        final String value = String.format(template, name.or(defaultName));
-        return new Saying(counter.incrementAndGet(), value);
-    }
 }
